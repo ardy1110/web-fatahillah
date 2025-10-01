2b7b9dc10aea7f352571b8bc053520db762edb3a
ardy1110 <ardia0908@gmail.com>
2025-10-01 13:32:43 +0700

    get category

---
+++ app/api/categories/route.js
@@ -0,0 +1,11 @@
+import { NextResponse } from "next/server";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+export async function GET() {
+    const categories = await prisma.category.findMany({
+        include: {products: true}
+    })
+    return NextResponse.json(categories)
+}
