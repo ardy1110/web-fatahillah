15f20b0ba2075601c0506edbc0f27c855ba32c34
ardy1110 <ardia0908@gmail.com>
2025-10-02 10:40:32 +0700

    add-fitur-updateProduct

--- app/api/products/[id]/route.js
+++ app/api/products/[id]/route.js
@@ -22,3 +22,27 @@ export async function DELETE(req, { params }) {
     );
   }
 }
+
+export async function PUT(req, {params}){
+  try {
+    const {id} = params;
+    const body = await req.json();
+    const updateProduct = await prisma.product.update({
+      where: {id: parseInt(id)},
+      data: {
+        name: body.name,
+        price: body.price,
+        description: body.description,
+      }
+    })
+    return NextResponse.json({
+      message: "Product updated successfully",
+      product: updateProduct,
+    })
+  } catch (error) {
+    return NextResponse.json({
+      error: "Failed to update product",
+      details: error.message,
+    },{status: 500})
+  }
+}
