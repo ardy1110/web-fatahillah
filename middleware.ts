585d42186e22bacb12db634eb45077e614b00916
ardy1110 <ardia0908@gmail.com>
2025-10-04 13:56:46 +0700

    add middleware

---
+++ middleware.ts
@@ -0,0 +1,16 @@
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+
+export function middleware(req: NextRequest){
+    const token = req.cookies.get('admin_token')
+
+    if(req.nextUrl.pathname.startsWith('/admin/dashboard') && !token){
+        return NextResponse.redirect(new URL('/admin/login', req.url))
+    }
+
+    return NextResponse.next()
+}
+
+export const config = {
+    matcher: ['/admin/dashboard:path*'],
+}
