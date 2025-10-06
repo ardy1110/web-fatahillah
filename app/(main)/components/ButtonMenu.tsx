ed9f0e732ddbe48f737b4ad5e5a924f86f31cfb4
ardy1110 <ardia0908@gmail.com>
2025-10-06 14:34:35 +0700

    gatau lah

--- app/(main)/components/ButtonMenu.tsx
+++ app/(main)/components/ButtonMenu.tsx
@@ -1,14 +1,10 @@
-import { BookOpen } from "lucide-react";
 import React from "react";
 
 const ButtonMenu = () => {
   return (
-    <div>
-      <button className="flex items-center gap-2 px-6 py-2 rounded-xl bg-[tomato] text-white text-xl cursor-pointer">
-        <BookOpen />
-        Menu
-      </button>
-    </div>
+    <button className="flex items-center gap-2 px-6 py-2 rounded-xl bg-[tomato] text-white text-xl cursor-pointer">
+      Menu
+    </button>
   );
 };
 
