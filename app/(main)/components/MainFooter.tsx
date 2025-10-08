ec13fb4a50aad34fbbc2f53f8edbed96adf298ec
ardy1110 <ardia0908@gmail.com>
2025-10-08 16:23:22 +0700

    footer

--- app/(main)/components/MainFooter.tsx
+++ app/(main)/components/MainFooter.tsx
@@ -2,8 +2,24 @@ import React from "react";
 
 const MainFooter = () => {
   return (
-    <footer className="flex items-center justify-center h-[50dvh] bg-amber-600 text-white text-3xl">
-      Footer Content
+    <footer className="h-[50dvh] bg-amber-600 text-white p-2">
+      <div className="grid grid-cols-2 md:grid-cols-4 gap-1.5">
+        <div>
+          <h1>Fatahillah Warkop</h1>
+          <p>Kopi nikmat, makanan enak, harga bersahabat</p>
+          
+        </div>
+        <div>
+          <h1>Toko</h1>
+        </div>
+        <div>
+          <h1>Contact Us</h1>
+        </div>
+        <div>
+          <h1>Buka Saat</h1>
+        </div>
+      </div>
+      <div className="border-t-2"></div>
     </footer>
   );
 };
