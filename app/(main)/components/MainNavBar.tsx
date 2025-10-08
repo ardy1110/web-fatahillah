f321f6763bc40c818fe963d3fd8f7e8987109681
Farhan Maulana <maulanafarhan210@gmail.com>
2025-10-08 15:37:23 +0700

    Logo Navbar

--- app/(main)/components/MainNavBar.tsx
+++ app/(main)/components/MainNavBar.tsx
@@ -4,6 +4,7 @@ import React, { useEffect, useState } from "react";
 import ButtonMenu from "./ButtonMenu";
 import { usePathname } from "next/navigation";
 import Link from "next/link";
+import Image from "next/image";
 
 const MainNavBar = () => {
   const pathname = usePathname();
@@ -21,22 +22,33 @@ const MainNavBar = () => {
 
   return (
     <nav
-      className={`fixed py-5 px-10 top-0 left-0 w-full z-50 duration-500 bg-transparent ${
+      className={`fixed p-10 top-0 left-0 w-full z-50 duration-500 bg-transparent ${
         isSolid ? "bg-white/90 shadow-md backdrop-blur-sm" : "bg-transparent"
       }`}
     >
-      <div className="flex items-center justify-between">
-        {/* Bagian kiri */}
-        <Link
-          href={"/"}
-          className={`text-2xl font-bold ${
-            !isSolid && pathname !== "/menu" ? "text-white" : "text-black"
-          }`}
-        >
-          ☕ LOGO
+      <div className="flex items-start justify-between">
+        <Link href="/" className="flex items-center space-x-4">
+          <div className="relative w-16 h-16">
+            <Image
+              src="/favicon.ico"
+              alt="Logo Fatahillah"
+              fill
+              className={`transition-all duration-200 ${
+                !isSolid && pathname !== "/menu"
+                  ? "invert brightness-0"
+                  : "invert brightness-200"
+              }`}
+            />
+          </div>
+          <div
+            className={`flex flex-col items-center text-2xl font-bold transition-all duration-200 ${
+              !isSolid && pathname !== "/menu" ? "text-white" : "text-black"
+            }`}
+          >
+            <h1>Warkop</h1>
+            <h1>Fatahillah</h1>
+          </div>
         </Link>
-        {/* Bagian kanan */}
-
         {pathname === "/" && <ButtonMenu />}
       </div>
     </nav>
