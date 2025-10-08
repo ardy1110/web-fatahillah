0528a3a02ee9cec412266b018754c65e2a061a52
Farhan Maulana <maulanafarhan210@gmail.com>
2025-10-08 14:06:55 +0700

    nth atur apa tddi

--- app/(main)/components/ButtonMenu.tsx
+++ app/(main)/components/ButtonMenu.tsx
@@ -1,11 +1,10 @@
 import { BookOpen } from "lucide-react";
 import Link from "next/link";
-import React from "react";
 
 const ButtonMenu = () => {
   return (
     <Link
-      href={"../menu"}
+      href={"/menu"}
       className="flex items-center gap-2 px-6 py-2 rounded-sm bg-amber-500 hover:bg-amber-600 text-white text-xl cursor-pointer"
     >
       <BookOpen />
--- app/(main)/menu/loading.tsx
+++ app/(main)/menu/loading.tsx
@@ -1,7 +1,7 @@
 // app/menu/loading.tsx
 export default function MenuLoading() {
   return (
-    <div className="flex items-center justify-center h-dvh">
+    <div className="flex items-center justify-center bg-black/30 backdrop-blur-sm h-dvh">
       <div className="animate-spin rounded-full h-10 w-10 border-4 border-gray-300 border-t-amber-600" />
     </div>
   );
