3927f012a98be3c372ef261176741ac5be9cd3b1
Farhan Maulana <maulanafarhan210@gmail.com>
2025-10-11 17:18:55 +0700

    Masih bergelud dengan navMenu

--- app/(main)/components/ContactMap.tsx
+++ app/(main)/components/ContactMap.tsx
@@ -2,18 +2,20 @@ import React from "react";
 
 const ContactMap = () => {
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2 gap-6 items-stretch justify-between px-6 mb-24">
+    <div className="grid grid-cols-1 md:grid-cols-2 gap-6 items-stretch justify-between px-6 md:px-12 mb-24">
       <div className="flex flex-col justify-between">
         <div>
-          <h2 className="pb-4 font-bold text-2xl">Kontak Kami</h2>
+          <h2 className="text-center md:text-start pb-4 font-bold text-2xl">
+            Kontak Kami
+          </h2>
 
-          <p className="text-base font-normal">
+          <p className="text-center md:text-start font-normal pb-4">
             Jangan ragu untuk menghubungi kami jika Anda memiliki pertanyaan,
             saran, atau ingin melakukan pemesanan. Kami siap melayani Anda
             dengan senang hati.
           </p>
         </div>
-        <address className="text-md space-y-2">
+        <address className="text-center md:text-start text-md space-y-2">
           <p>
             <span className="font-semibold">Email:</span>{" "}
             <span>info@warkopfatahillah.com</span>
@@ -29,11 +31,12 @@ const ContactMap = () => {
         </address>
       </div>
       <div>
-        <h2 className="pb-4 font-bold text-2xl">Lokasi Kami</h2>
+        <h2 className="text-center md:text-start pb-4 font-bold text-2xl">
+          Lokasi Kami
+        </h2>
         <iframe
-          width={600}
           height={250}
-          className="rounded-2xl shadow-md"
+          className="w-full rounded-2xl shadow-md"
           src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3971.2351077229428!2d95.30465977420894!3d5.532101994448091!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x30403b0003db853d%3A0x180858a4a9d2d4fa!2sWarkop%20Fatahillah!5e0!3m2!1sid!2sid!4v1759661072583!5m2!1sid!2sid"
           loading="lazy"
         ></iframe>
--- app/(main)/menu/MenuClient.tsx
+++ app/(main)/menu/MenuClient.tsx
@@ -13,7 +13,7 @@ interface Product {
 interface Toko {
   id: number;
   name: string;
-  categories?: string;
+  categories?: string | null;
   products?: Product[];
 }
 
@@ -21,7 +21,7 @@ export default function MenuClient({ tokoList }: { tokoList: Toko[] }) {
   // const [selectedToko, setSelectedToko] = useState<Toko | null>(
   //   tokoList[0] ?? null
   // );
-  console.log(tokoList[0].products);
+  // console.log(tokoList[0].products);
 
   return (
     // Tambah scroll
@@ -68,20 +68,22 @@ export default function MenuClient({ tokoList }: { tokoList: Toko[] }) {
 
       {/* Navbar Bawah */}
       <footer className="absolute bottom-0 left-0 w-full flex justify-center">
-        <div className="bg-amber-600 p-2 m-4 rounded-xl shadow-md inline-flex items-center justify-center gap-4 px-10">
-          {tokoList.map((product) => (
-            <div
-              key={product.id}
-              className="relative bg-white w-16 h-16 rounded-full text-sm"
-            >
-              <Image
-                src={`/${product.name}.jpg`}
-                alt="Logo Toko"
-                fill
-                className="object-cover rounded-full"
-              />
-            </div>
-          ))}
+        <div className="relative p-3 m-4 rounded-2xl shadow-lg inline-flex items-center justify-center gap-4 px-10 overflow-hidden backdrop-blur-md bg-amber-500/30 border border-white/20">
+          <div className="flex gap-4 overflow-x-auto sm:overflow-visible max-w-full scrollbar-none">
+            {tokoList.map((product) => (
+              <div
+                key={product.id}
+                className="relative bg-white w-16 h-16 rounded-xl text-sm shadow-md shrink-0 hover:scale-105 transition-transform duration-300"
+              >
+                <Image
+                  src={`/${product.name}.jpg`}
+                  alt="Logo Toko"
+                  fill
+                  className="object-cover rounded-xl"
+                />
+              </div>
+            ))}
+          </div>
         </div>
       </footer>
     </main>
