3773306c4b6c41aff5827dd137300a5e5faa08ed
ardy1110 <ardia0908@gmail.com>
2025-10-11 14:39:07 +0700

    count tokoProduct

--- app/admin/(dashboard)/page.tsx
+++ app/admin/(dashboard)/page.tsx
@@ -1,21 +1,38 @@
-export default function AdminPage() {
+
+
+interface Toko {
+  id: number
+  name: string
+  categories?: string
+  products?: {id: number}[]
+
+}
+
+export default async function AdminPage() {
+ 
+  const res = await fetch("http://localhost:3000/api/toko");
+  const datas = await res.json();
+  const totalToko = datas.length;
+  const totalProduk = datas.reduce(
+    (sum: number, toko: Toko) => sum + (toko.products?.length || 0),
+    0
+  );
+
   return (
-    <>
-      {/* Sidebar */}
-      {/* Main content */}
-      <main className="flex-1 p-6">
-        <h1 className="text-2xl font-bold mb-4">Dashboard Admin</h1>
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
-          <div className="bg-white p-6 rounded-xl shadow">
-            <h2 className="text-gray-500 text-sm">Total Toko</h2>
-            <p className="text-3xl font-bold mt-2">6</p>
-          </div>
-          <div className="bg-white p-6 rounded-xl shadow">
-            <h2 className="text-gray-500 text-sm">Total Produk</h2>
-            <p className="text-3xl font-bold mt-2">24</p>
-          </div>
+    <main className="flex-1 p-6 space-y-6">
+      <h1 className="text-2xl font-bold mb-4">Dashboard Admin</h1>
+
+      {/* Statistik */}
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+        <div className="bg-white p-6 rounded-xl shadow">
+          <h2 className="text-gray-500 text-sm">Total Toko</h2>
+          <p className="text-3xl font-bold mt-2">{totalToko}</p>
         </div>
-      </main>
-    </>
+        <div className="bg-white p-6 rounded-xl shadow">
+          <h2 className="text-gray-500 text-sm">Total Produk</h2>
+          <p className="text-3xl font-bold mt-2">{totalProduk}</p>
+        </div>
+      </div>
+    </main>
   );
 }
