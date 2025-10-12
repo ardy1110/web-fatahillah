24712f65beb5012a664e5d281cdc533f7970536f
Farhan Maulana <maulanafarhan210@gmail.com>
2025-10-12 15:44:10 +0700

    admin page

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -7,30 +7,26 @@ interface Store {
 }
 
 export default async function Admin() {
-  try {
-    const res = await fetch("http://localhost:3000/api/store", {
-      cache: "no-store", // atau 'force-cache' sesuai kebutuhan
-    });
+  const res = await fetch("http://localhost:3000/api/store", {
+    // cache: "no-store", 
+  });
 
-    if (!res.ok) {
-      throw new Error("Failed to fetch");
-    }
-
-    const datas: Store[] = await res.json();
-
-    return (
-      <>
-        <div>Admin</div>
-        <ul>
-          {datas.map((data) => (
-            <li key={data.id}>
-              <Link href={`/admin/${data.id}`}>{data.name}</Link>
-            </li>
-          ))}
-        </ul>
-      </>
-    );
-  } catch (error) {
-    return <div>Error loading data</div>;
+  if (!res.ok) {
+    throw new Error("Failed to fetch");
   }
+
+  const datas: Store[] = await res.json();
+
+  return (
+    <>
+      <div>Admin</div>
+      <ul>
+        {datas.map((data) => (
+          <li key={data.id}>
+            <Link href={`/admin/${data.id}`}>{data.name}</Link>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
 }
