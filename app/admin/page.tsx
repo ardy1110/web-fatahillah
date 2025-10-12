e26eced45809146da1f6c4db4fbb182f58ca5e81
Farhan Maulana <maulanafarhan210@gmail.com>
2025-10-12 17:51:02 +0700

    page admin

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import Link from "next/link";
 
 // Tambahkan type untuk array
@@ -16,18 +17,28 @@ export default async function Admin() {
   }
 
   const datas: Store[] = await res.json();
-  console.log(datas);
 
   return (
-    <>
-      <div>Admin</div>
-      <ul>
+    <main className="px-36 py-12">
+      <h1 className="flex justify-center font-bold text-2xl pb-12">
+        Dashboard
+      </h1>
+      <div className="grid grid-cols-3 gap-12 w-full">
         {datas.map((data) => (
-          <li key={data.id}>
-            <Link href={`/admin/${data.id}`}>{data.name}</Link>
-          </li>
+          <Link
+            key={data.id}
+            href={`/admin/${data.id}`}
+            className="relative h-36"
+          >
+            <Image
+              src="/IconBlack.jpeg"
+              alt="Logo Toko"
+              fill
+              className="object-cover rounded-xl"
+            />
+          </Link>
         ))}
-      </ul>
-    </>
+      </div>
+    </main>
   );
 }
