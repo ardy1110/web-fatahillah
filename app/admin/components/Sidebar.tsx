eb4307059f01d94994ce35c82fee8461057a82fc
Farhan Maulana <maulanafarhan210@gmail.com>
2025-10-14 15:15:31 +0700

    SideBarAdmin

--- app/admin/components/Sidebar.tsx
+++ app/admin/components/Sidebar.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { LogOut } from "lucide-react";
-import NavButton from "./NavButton";
 import AddStore from "./AddStore";
+import ListStores from "./NavButton";
 
 const SidebarAdmin = async () => {
   const res = await fetch("http://localhost:3000/api/store");
@@ -12,10 +12,10 @@ const SidebarAdmin = async () => {
     <aside className="fixed top-0 left-0 w-64 h-dvh bg-white shadow-md flex flex-col">
       <div className="px-6 py-4 border-b flex justify-between items-center">
         <h2 className="text-xl font-bold text-amber-600">Admin Panel</h2>
-        <AddStore/>
+        <AddStore />
       </div>
-      
-      <NavButton stores={stores} />
+
+      <ListStores stores={stores} />
 
       <div className="p-4 border-t">
         <button className="flex items-center gap-2 text-red-500 hover:text-red-800 cursor-pointer">
