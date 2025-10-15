58a4b984fc74f749dd7fe4bf25eff9d780219389
Farhan Maulana <maulanafarhan210@gmail.com>
2025-10-15 15:37:16 +0700

    tipis2

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,6 +1,6 @@
 export default async function AdminPage() {
   return (
-    <div className="flex items-center justify-center h-screen text-gray-600">
+    <div className="flex items-center justify-center h-screen text-gray-600 italic">
       Belum ada toko
     </div>
   );
