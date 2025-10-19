67c27a616ae2b96fd8f7043bd5a45b7b4e7a4469
Farhan Maulana <maulanafarhan210@gmail.com>
2025-10-19 17:19:47 +0700

    tambah data

--- app/(main)/menu/MenuClient.tsx
+++ app/(main)/menu/MenuClient.tsx
@@ -22,13 +22,13 @@ export default function MenuClient({ stores }: { stores: Store[] }) {
   }
 
   return (
-    <main className="flex flex-col h-dvh pb-24">
+    <main className="flex flex-col h-dvh">
       <div className="flex-1 overflow-y-auto scrollbar-none pt-12 md:pt-6">
         <header className="text-center py-6 px-4">
           <h1 className="text-3xl font-bold text-black">{activeStore.name}</h1>
         </header>
 
-        <div className="px-4">
+        <div className="px-4 pb-32">
           {activeStore.name === "Warkop Fatahillah" ? (
             <MenuWF store={activeStore} />
           ) : (
